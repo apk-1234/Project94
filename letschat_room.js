166dbabfeb01ae5361d3b50cb04298d2a345fd43
apk-1234 <81460947+apk-1234@users.noreply.github.com>
2021-08-07 13:09:55 +0530

    Add files via upload

---
+++ letschat_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDh4rBCJuOqqMLermsOvJ982pqbeohri-Y",
+    authDomain: "letschat-adf0b.firebaseapp.com",
+    databaseURL: "https://letschat-adf0b-default-rtdb.firebaseio.com",
+    projectId: "letschat-adf0b",
+    storageBucket: "letschat-adf0b.appspot.com",
+    messagingSenderId: "533687760016",
+    appId: "1:533687760016:web:ebd4fe27cfc6b6d4331415"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
